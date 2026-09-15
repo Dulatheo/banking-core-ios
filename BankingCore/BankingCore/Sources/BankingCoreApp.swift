c1310a9d11e46d4ad3a18d15d0337fd5574106ef
Dulat Yessilbayev <d.yessilbayev@gmail.com>
2026-09-15 12:19:28 +0500

    INitial commit

---
+++ BankingCore/BankingCore/Sources/BankingCoreApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct BankingCoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BankingCore/Project.swift
@@ -0,0 +1,19 @@
+import ProjectDescription
+
+let project = Project(
+    name: "BankingCore",
+    targets: [
+        .target(
+            name: "CoreNetworking",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "com.dulatheo.bankingcore.networking",
+            deploymentTargets: .iOS("16.4"),
+            infoPlist: .default,
+            sources: [
+                "Modules/Networking/Sources/**"
+            ],
+            dependencies: []
+        )
+    ]
+)
